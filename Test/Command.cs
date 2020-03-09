b34b0152449a9a14e28b1d588be66252fb71116b
imkcrevit <scgq425@gmail.com>
2020-03-09 14:53:42 +0800

    修改

--- Test/Command.cs
+++ Test/Command.cs
@@ -12,7 +12,7 @@ namespace Test
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            
+            TaskDialog.Show("Revit", "Hello World!");
             return Result.Succeeded;
         }
     }
