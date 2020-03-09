bf93e39b3b15b0021596d448f6b6cd4ac800087c
imkcrevit <scgq425@gmail.com>
2020-03-09 14:47:39 +0800

    添加项目文件。

---
+++ Test/Command.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+
+namespace Test
+{
+    public class Command : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            
+            return Result.Succeeded;
+        }
+    }
+}
